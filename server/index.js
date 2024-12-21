ef3a01ec4aae92591dc1a858a8f2621c47bb1327
praneeth <praneethchakka23@gmail.com>
2024-12-21 22:05:12 +0530

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,24 +1,27 @@
 const express = require('express');
 require('dotenv').config();
 const generator = require('./geminiAi/geminiApi')
+const mongoose = require('mongoose');
 
 
 const app = express();
 const PORT = process.env.PORT ||3000;
+let fullPrompt = '';
+const connection = process.env.mongodb
 
+mongoose.connect(connection).then(console.log("mongodb connected successfully")).catch((err)=>{
+    console.log(err);
+});
 app.use(express.json());
 
 app.post('/askAi',async(req,res)=>{
      const { userPrompt ,mailId } = req.body;
-    const fullPrompt =  await generator(userPrompt);
-    if(!fullPrompt){
+     fullPrompt =  await generator(userPrompt);
+    if(fullPrompt){
     res.status(200).json(fullPrompt);
     }else{
         res.status(400).json("give correct prompt");
     }
-})
-app.get('/',(req,res)=>{
-    res.json("hello");
 })
 app.listen(PORT , ()=>{
     console.log(`the app is listening to`,PORT);
