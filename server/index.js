4ed0e9479a7c7c7429db3327720802303c07305f
praneeth <praneethchakka23@gmail.com>
2024-12-21 22:55:53 +0530

    changed index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT ||3000;
-let fullPrompt = '';
 const connection = process.env.mongodb
 
 mongoose.connect(connection).then(console.log("mongodb connected successfully")).catch((err)=>{
@@ -16,7 +15,7 @@ app.use(express.json());
 
 app.post('/askAi',async(req,res)=>{
      const { userPrompt ,mailId } = req.body;
-     fullPrompt =  await generator(userPrompt);
+      const fullPrompt =  await generator(userPrompt);
     if(fullPrompt){
     res.status(200).json(fullPrompt);
     }else{
