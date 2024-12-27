d6514bcdb7674579705424c34116bbd871dcd8df
praneeth <praneethchakka23@gmail.com>
2024-12-27 17:38:33 +0530

    completed working on sendMail and setUp

--- server/components/sendMail.js
+++ server/components/sendMail.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const transporterFunction = require('../utils/nodemailer');
+const MailInfo = require('../schemas/mailInfoSchema');
+const UserInfo = require('../schemas/userInfoSchema');
 
 
-router.post('/',(req,res)=>{
-    const {senderEmailId , appPassword , reciverEmailId , mailSubject , fullPrompt} = req.body;
+
+router.post('/',async(req,res)=>{
+    const { email ,reciverEmailId , mailSubject , fullPrompt} = req.body;
+    if (!email || !reciverEmailId || !mailSubject || !fullPrompt) {
+        return res.status(400).json({ message: "Missing required fields" });
+      }
+    const user =  await UserInfo.findOne({email});
+    if(!user){
+        return res.status(400).json({message:"no user exists"});
+    }
+    const MailInfoDetails = await MailInfo.findOne({userDetails : user._id})
+    const senderEmailId = MailInfoDetails.senderEmailId;
+    const appPassword = MailInfoDetails.appPassword;
+
+
     const ret = transporterFunction(senderEmailId , appPassword , reciverEmailId , mailSubject , fullPrompt);
+
+
     if(ret){
         res.status(200).json({message:"the mail is sent to the user successfully"});
     }else{
---
+++ server/components/setUp.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const MailInfo = require('../schemas/mailInfoSchema');
+const UserInfo = require("../schemas/userInfoSchema")
+
+router.post("/",async(req,res)=>{
+    const {senderEmailId , appPassword ,email} = req.body;
+    try {
+        const user = await UserInfo.findOne({email:email});
+        if(!user){
+            return res.status(200).json({message:"no user found"});
+        }
+        const newMailInfo = new MailInfo({
+            senderEmailId ,
+            appPassword,
+            userDetails:user._id,
+        })
+        try {
+            console.log("the data before storing:",newMailInfo);
+        await newMailInfo.save();
+        res.status(200).json("added successfully");
+        } catch (error) {
+            console.log("Error:",error);
+            res.status(400).json("something is wrong")
+        }
+        console.log("the data before storing:",newMailInfo);
+        await newMailInfo.save();
+    } catch (error) {
+        console.log("error:",error);
+    }
+})
+module.exports = router;
