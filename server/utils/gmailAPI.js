e5a5e9b38fc4563755a1d557750db38e30429703
vaibhav <23211A04p8@bvrit.ac.in>
2025-03-29 23:51:29 +0530

    google secret

--- server/utils/gmailAPI.js
+++ server/utils/gmailAPI.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken')
 const getNewAccessToken = async (refreshToken) => {
   const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET
+    process.env.GOOGLE_SECRET
   );
   oauth2Client.setCredentials({ refresh_token: refreshToken });
 
